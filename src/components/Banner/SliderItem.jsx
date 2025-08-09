8c029eb369f7c8887f27fb63f23d836884ad2e28
kimhyejeong <rla157626@gmail.com>
2025-08-10 00:56:50 +0900

    style(banner): SliderItem spring 감쇠 조정으로 튕김 1회로 자연스럽게 변경

--- src/components/Banner/SliderItem.jsx
+++ src/components/Banner/SliderItem.jsx
@@ -6,14 +6,24 @@ const boxVariants = {
   hover: {
     scale: 1.3,
     y: -60,
-    transition: { delay: 0.5, duration: 0.1, type: "tween" },
+    transition: {
+      type: "spring",
+      damping: 30,
+      stiffness: 300,
+      restDelta: 0.5,
+    },
   },
 };
 
 const infoVariants = {
   hover: {
     opacity: 1,
-    transition: { delay: 0.5, duration: 0.1, type: "tween" },
+    transition: {
+      type: "spring",
+      damping: 30,
+      stiffness: 300,
+      restDelta: 0.5,
+    },
   },
 };
 
@@ -25,7 +35,6 @@ const SliderItem = ({ item, onBoxClicked, getTitle, getItemType }) => (
     initial="normal"
     variants={boxVariants}
     onClick={() => onBoxClicked(item.id, getItemType(item))}
-    transition={{ type: "tween" }}
     className="relative h-32 cursor-pointer first:origin-center-left last:origin-center-right overflow-visible"
   >
     <img
@@ -38,7 +47,7 @@ const SliderItem = ({ item, onBoxClicked, getTitle, getItemType }) => (
     />
     <motion.div
       variants={infoVariants}
-      className="p-2 bg-gray-800 opacity-0 absolute w-full bottom-0 z-10"
+      className="p-2 opacity-0 absolute w-full bottom-0 z-10 bg-gradient-to-t from-violet-900/80 via-fuchsia-800/50 to-transparent"
     >
       <h4 className="text-center text-lg text-white font-light">{getTitle(item)}</h4>
     </motion.div>
