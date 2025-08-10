328b35a1f2d0578c52c8772683b98e62be9592b3
kimhyejeong <rla157626@gmail.com>
2025-08-10 21:47:30 +0900

    refactor: 사용하지 않는 animations.js 파일 제거하고 DetailLayout에 애니메이션 인라인
    - DetailLayout에서만 사용되던 animations.js 유틸리티 파일 삭제
    - 사용되지 않는 fadeIn 애니메이션 variant 제거

--- src/components/Detail/DetailLayout.jsx
+++ src/components/Detail/DetailLayout.jsx
@@ -1,10 +1,29 @@
 import { motion } from "framer-motion";
 import { mapMediaData } from "../../utils/mediaDataMapper";
-import { detailPageVariants as variants } from "../../utils/animations";
 import PrimaryDetails from "../../components/Detail/PrimaryDetails";
 
 const MediaDetailLayout = ({ data, type = "movie", children }) => {
   const media = mapMediaData(data || {}, type);
+
+  const detailPageVariants = {
+    container: {
+      animate: {
+        transition: {
+          staggerChildren: 0.1,
+        },
+      },
+    },
+    fadeInUp: {
+      initial: { opacity: 0, y: 20 },
+      animate: { opacity: 1, y: 0 },
+      transition: { duration: 0.6 },
+    },
+    scaleIn: {
+      initial: { scale: 0.8, opacity: 0 },
+      animate: { scale: 1, opacity: 1 },
+      transition: { duration: 0.6 },
+    },
+  };
 
   return (
     <div className="bg-black min-h-screen">
@@ -17,8 +36,8 @@ const MediaDetailLayout = ({ data, type = "movie", children }) => {
 
       <div className="relative z-10 pt-32 pb-16">
         <div className="container mx-auto px-4">
-          <motion.div className="flex flex-col" variants={variants.container} initial="initial" animate="animate">
-            <PrimaryDetails media={media} variants={variants} />
+          <motion.div className="flex flex-col" variants={detailPageVariants.container} initial="initial" animate="animate">
+            <PrimaryDetails media={media} variants={detailPageVariants} />
           </motion.div>
         </div>
       </div>
